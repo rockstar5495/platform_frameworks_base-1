GeoZac/frameworks_base-1

core: Bring back UdfpsUtils for Udfps config checking

core/java/com/android/internal/util/evolution/udfps/UdfpsUtils.java
@@ -1,0 +1,29 @@
+/*
+* Copyright (C) 2020 The Pixel Experience Project
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.evolution.udfps;
+
+import android.content.Context;
+
+import com.android.internal.util.ArrayUtils;
+
+public class UdfpsUtils {
+    public static boolean hasUdfpsSupport(Context context) {
+        int[] udfpsProps = context.getResources().getIntArray(
+                com.android.internal.R.array.config_udfps_sensor_props);
+
+        return !ArrayUtils.isEmpty(udfpsProps);
+    }
+}

rockstar5495/platform_frameworks_base-1

Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
Signed-off-by: Pratyaksh Bharadwaj <pratyakshom.05@gmail.com>

core/java/com/android/internal/util/colt/udfps/UdfpsUtils.java
@@ -1,0 +1,29 @@
+/*
+* Copyright (C) 2022 Colt-Enigma
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.android.internal.util.colt.udfps;
+
+import android.content.Context;
+
+import com.android.internal.util.ArrayUtils;
+
+public class UdfpsUtils {
+    public static boolean hasUdfpsSupport(Context context) {
+        int[] udfpsProps = context.getResources().getIntArray(
+                com.android.internal.R.array.config_udfps_sensor_props);
+
+        return !ArrayUtils.isEmpty(udfpsProps);
+    }
+}